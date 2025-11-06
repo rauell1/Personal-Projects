a0e5e020d6e6e9c265ca8bde03d1f6237068e187
Roy Okola Otieno <royokola3@gmail.com>
2025-11-06 22:36:44 +0300

    Eloquent JavaScript Test Update

---
+++ Coding in Color/Eloquent JavaScript/ChessBoard.js
@@ -0,0 +1,5 @@
+// Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines.
+// At each position of the grid there is either a space or a "#" character.
+// The characters should form a chessboard pattern.
+
+// When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
---
+++ Coding in Color/Eloquent JavaScript/LoopingTriangle.js
@@ -0,0 +1,11 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###      
+// ####
+// #####
+// ######
+// #######
+
+// IT MAY BE USEFUL TO KNOW THAT YOU CAN FIND THE LENGHT OF A STRING BY WRITING .LENGTH AFTER IT.
+
