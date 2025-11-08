6809251de1d7773d589101bee0e089d7e2c92d87
Roy Okola Otieno <royokola3@gmail.com>
2025-11-08 16:09:28 +0300

    Test 4: Functions example

--- Coding in Color/Test4.js
+++ Coding in Color/Test4.js
@@ -1,3 +1,18 @@
+const car2 = {
+    make: 'Honda',
+    model: 'Civic',
+    year: 2019,
+    color: 'black'
+};      
+
+const keys = Object.keys(car2)
+
+for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    console.log(`${key}: ${car2[key]}`);
+}
+
+
 let car = {
     make: 'Toyota', 
     model: 'Camry',
