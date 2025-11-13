ef39f53ad535df4e0e381251b9d0b21f218caa86
Roy Okola Otieno <royokola3@gmail.com>
2025-11-13 08:32:47 +0300

    Test branch

--- Coding in Color/Recursive.js
+++ Coding in Color/Recursive.js
@@ -45,3 +45,28 @@ console.log(checkPalindrome("racecar")); // Outputs: true
 console.log(checkPalindrome("hello")); // Outputs: false    
 console.log(checkPalindrome("madam")); // Outputs: true
 console.log(checkPalindrome("step on no pets")); // Outputs: true
+
+// Fibonacci Sequence
+function recursiveFibonacci(num) {
+    if (num <= 0){  
+        return 0;
+    } else if (num == 1){
+        return 1;
+    } else {
+        return recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2);
+    }
+}
+
+console.log(recursiveFibonacci(6)); // Outputs: 8
+console.log(recursiveFibonacci(10)); // Outputs: 55 
+console.log(recursiveFibonacci(0)); // Outputs: 0
+console.log(recursiveFibonacci(1)); // Outputs: 1
+console.log(recursiveFibonacci(5)); // Outputs: 5   
+console.log(recursiveFibonacci(7)); // Outputs: 13
+console.log(recursiveFibonacci(8)); // Outputs: 21
+console.log(recursiveFibonacci(9)); // Outputs: 34
+console.log(recursiveFibonacci(4)); // Outputs: 3
+console.log(recursiveFibonacci(3)); // Outputs: 2
+console.log(recursiveFibonacci(2)); // Outputs: 1
+console.log(recursiveFibonacci(11)); // Outputs: 89
+console.log(recursiveFibonacci(12)); // Outputs: 144
