892654e6fe757fd644d5ee096abf6abde8f0fa10
Roy Okola Otieno <royokola3@gmail.com>
2025-11-18 11:17:02 +0300

    Update

--- Coding in Color/HoF.js
+++ Coding in Color/HoF.js
@@ -23,31 +23,3 @@ let numbers = [1, 2, 3, 4, 5];
 console.log(processArray(numbers, double)); // [2, 4, 6, 8, 10
 console.log(processArray(numbers, square)); // [1, 4, 9, 16, 25
 
-
-// How can higher order functions be utilized to filter an array based on specific criteria?
-// Provide an example where an array of numbers is filtered to include only even numbers and add numbers greater than three.
-
-// Higher order Function for filtering
-function filterArray(arr, callback) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (callback(arr[i])) {
-            result.push(arr[i]);
-        }
-        return result;
-    }
-        // Callback function to check for even numbers
-        function isEven(num) {
-            return num % 2 === 0;
-        }       
-        // Callback function to check for numbers greater than three
-        function isGreaterThanThree(num) {
-            return num > 3;
-        }
-
-        // Using the higher-order function to filter even numbers
-        let numberrs = [1, 2, 3, 4, 5, 6];
-        console.log(filterArray(numberrs, isEven)); // [2, 4, 6]
-        console.log(filterArray(numberrs, isGreaterThanThree)); // [4, 5, 6]
-
-    }
